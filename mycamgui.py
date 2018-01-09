d50a6b0fd76126e57dc043b4520381684d9d759e
Per Engström <perengstrom.git@gmail.com>
2018-01-09 21:19:47 +0000

    Added MyCamCanvas and MyCamMenu to mycamgui.py

--- mycamgui.py
+++ mycamgui.py
@@ -13,56 +13,84 @@ def do_grid(widget, grid):
 	widget.grid(**grid)
 	return widget
 
-class MainApplication:
-	def __init__(self, master, mycam):
-		self.master = master
-		self.mycam = mycam
+class MyCamMenu(tk.Frame):
+	def __init__(self, master, app):
+		tk.Frame.__init__(self, master)
 
-		self.frame = tk.Frame(self.master)
+		self.widget1 = self.build_label("Controls", grid={"columnspan":1})
+		self.widget2 = self.build_button("Calibrate", app.cmd_calibrate, grid={"columnspan":2})
+		self.widget3 = self.build_labelframe("Capture", grid={"sticky":"EW"})
+		self.widget3.x = self.build_checkbox("Live", root = self.widget3)
+		self.widget3.y = self.build_button("Now", app.cmd_capture, root = self.widget3, grid={"column":1, "row":0, "sticky":"E"})
+		self.widget4 = self.build_button("Save...", app.cmd_save, grid={"columnspan":1})
+		self.widget5 = self.build_label("Settings", grid={"columnspan":1})
+		self.widget6 = self.build_labelframe("Mode")
+		self.widget6.x = self.build_checkbox("Default", root = self.widget6)
+		self.widget6.y = self.build_entry(root = self.widget6, grid={"column":1, "row":0})
+		self.widget7 = self.build_labelframe2("Exposure")
+		self.widget8 = self.build_labelframe2("ISO")
+		self.widget9 = self.build_labelframe2("Delay")
+		self.widget10 = self.build_button("Exit", app.cmd_exit, grid={"columnspan":2})
 
-		self.widget1 = self.build_label("Controls", grid={"columnspan":2})
-		self.widget2 = self.build_button("Calibrate", self.cmd_calibrate, grid={"columnspan":2})
-		self.widget3x= self.build_checkbox("Continous")
-		self.widget3 = self.build_button("Capture", self.cmd_capture, grid={"column":1, "row":2})
-		self.widget4 = self.build_button("Save...", self.cmd_save, grid={"columnspan":2})
+	def build_labelframe2(self, text, root = None, grid = {}):
+		if (root is None): root = self
+		frame = ttk.LabelFrame(root, text=text)
+		frame.checkbox = self.build_checkbox("Default", root = frame)
+		frame.entry = self.build_entry(root = frame, grid={"column":1, "row":0})
+		return do_grid(frame, grid)
 
-		self.widget5 = self.build_label("Settings", grid={"columnspan":2})
-		self.widget6x = self.build_checkbox("Default")
-		self.widget6 = self.build_entry(grid={"column":1, "row":5, "sticky":tk.W})
-		self.widget7x = self.build_checkbox("Default")
-		self.widget7 = self.build_entry(grid={"column":1, "row":6})
-		self.widget8x = self.build_checkbox("Default")
-		self.widget8 = self.build_entry(grid={"column":1, "row":7})
-		self.widget9x = self.build_checkbox("Default")
-		self.widget9 = self.build_entry(grid={"column":1, "row":8})
+	def build_labelframe(self, text, root = None, grid = {}):
+		if (root is None): root = self
+		frame = ttk.LabelFrame(root, text=text)
+		return do_grid(frame, grid)
 
-		self.widget10 = self.build_button("Exit", self.cmd_exit, grid={"columnspan":2})
-		self.w = tk.Canvas(self.frame, width=960, height=540)
-		self.w.grid(column=10, row=0, rowspan=10)
-
-		self.pilimage = None
-		self.canimage = self.w.create_image((0, 0), anchor=tk.NW)
-		self.cantime = self.w.create_text((20, 20), anchor=tk.NW, fill="white")
-
-		self.frame.pack()
-
-	def build_label(self, text, grid = {}):
-		label = ttk.Label(self.frame, text=text)
+	def build_label(self, text, root = None, grid = {}):
+		if (root is None): root = self
+		label = ttk.Label(root, text=text)
 		return do_grid(label, grid)
 
-	def build_button(self, text, command, grid = {}):
-		button = ttk.Button(self.frame, text=text, command=command)
+	def build_button(self, text, command, root = None, grid = {}):
+		if (root is None): root = self
+		button = ttk.Button(root, text=text, command=command)
 		return do_grid(button, grid)
 
-	def build_entry(self, grid = {}):
-		entry = ttk.Entry(self.frame)
+	def build_entry(self, root = None, grid = {}):
+		if (root is None): root = self
+		entry = ttk.Entry(root)
 		return do_grid(entry, grid)
 
-	def build_checkbox(self, text, grid = {}):
-		var = tk.IntVar()
-		checkbox = tk.Checkbutton(self.frame, text=text, variable=var)
+	def build_checkbox(self, text, root = None, var = None, grid = {}):
+		if (root is None): root = self
+		if (var is None): var = tk.IntVar()
+		checkbox = tk.Checkbutton(root, text=text, variable=var)
 		checkbox.var = var
 		return do_grid(checkbox, grid)
+
+class MyCamCanvas(tk.Canvas):
+	def __init__(self, master, *args, **kwargs):
+		tk.Canvas.__init__(self, master, *args, **kwargs)
+
+		self.canimage = self.create_image((0, 0), anchor=tk.NW)
+		self.cantime = self.create_text((20, 20), anchor=tk.NW, fill="white")
+
+	def set_image(self, pil_image):
+		self.image = ImageTk.PhotoImage(pil_image)
+		self.itemconfig(self.canimage, image=self.image)
+
+	def set_time(self, timestamp):
+		self.itemconfig(self.cantime, text=timestamp)
+
+class MainApplication(tk.Frame):
+	def __init__(self, master, mycam):
+		tk.Frame.__init__(self, master)
+		self.mycam = mycam
+
+		self.menu = MyCamMenu(self, self)
+		self.menu.grid()
+		self.w = MyCamCanvas(self, width=960, height=540)
+		self.w.grid(column=10, row=0)
+
+		self.pilimage = None
 
 	def cmd_test(self):
 		print("Test")
@@ -77,19 +105,17 @@ class MainApplication:
 		self.pilimage = self.mycam.capture_image()
 		size = (960, 540)
 		self.pilimage = self.pilimage.resize(size, Image.ANTIALIAS)
-		self.image = ImageTk.PhotoImage(self.pilimage)
-		self.w.itemconfig(self.canimage, image=self.image)
+
+		self.w.set_image(self.pilimage)
 
 		self.timestamp = get_timestamp()
-		self.w.itemconfig(self.cantime, text=self.timestamp)
+		self.w.set_time(self.timestamp)
 
 	def cmd_set_mode(self):
 		print("Set mode")
 
 	def cmd_save(self):
 		if self.pilimage is not None:
-			#now = datetime.datetime.now()
-			#timestamp = now.strftime("%Y%m%d_%H%M%S")
 			filename = "{}.png".format(self.timestamp)
 			self.pilimage.save(filename)
 			print("Save '{}'".format(filename))
@@ -108,6 +134,7 @@ def main():
 	mycam = MyCamera('auto', 0)
 	root = tk.Tk()
 	app = MainApplication(root, mycam)
+	app.pack()
 	root.mainloop()
 
 if __name__ == "__main__":
